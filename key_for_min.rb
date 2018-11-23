8f2eccaee8a2488999938efbd03eac59330b8773
Nick Inman <nick@theoatmeal.com>
2018-11-23 18:58:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  next_value = 0
+  next_value = nil
   next_key = nil
   if name_hash.empty? == true
      return nil
