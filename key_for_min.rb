95f30c2cf0be4e2c639f48f11379a14b593a1100
Nick Inman <nick@theoatmeal.com>
2018-11-23 18:52:59 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,9 @@
 def key_for_min_value(name_hash)
   next_value = 0
   next_key = ""
+  if name_hash.empty? == true
+     return nil
+   end
  name_hash.each do |key, value|
    if value < next_value
      next_value = value
